d5763b91e83cf6d27bee6c7a50c689f108f8bd33
FranckPedron <franckyp1+rapports_git@gmail.com>
2022-07-17 16:09:40 +0200

    Init du serveur

---
+++ index.js
@@ -0,0 +1,11 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('<h1>Welcome!</h1>');
+})
+
+const PORT= process.env.PORT || 3000;
+
+app.listen(PORT, () => { console.log('listening on port ' + PORT); });
